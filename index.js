e38caf2517bf352d171b6bc649e37e93352380cf
Bharadwaj <rbharadwaj022@gmail.com>
2025-07-02 00:13:11 +0530

    .gitignore

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 require("dotenv").config();
 
 function getRoomUserCount(room) {
