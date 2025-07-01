471712afe4ad4a4ab37998b5519ebd2cb7cbcb20
Bharadwaj <rbharadwaj022@gmail.com>
2025-07-02 01:09:51 +0530

    .gitignore

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const http = require("http");
 const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+  transports: ["websocket", "polling"],
+});
 const port = process.env.PORT || 8000;
 require("dotenv").config();
 
